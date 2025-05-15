81b5c0891454a994cec877694bd3285121949436
sojeongw <sojeongw.official@gmail.com>
2025-05-15 12:45:04 +0900

    12.3. 라우팅 설정하기

---
+++ section12/src/App.jsx
@@ -0,0 +1,24 @@
+import Home from "./pages/Home";
+import Diary from "./pages/Diary";
+import New from "./pages/New";
+import NotFound from "./pages/NotFound";
+import { Routes, Route } from "react-router-dom";
+
+// "/": 모든 일를 조회하는 home
+// "/new": 새로운 일기를 작성
+// "/diary": 일기를 상세히 조회
+
+function App() {
+  return (
+    // routes 컴포넌트 안에는 route 컴포넌트만 들어갈 수 있다.
+    // routes 밖에 있는 컴포넌트는 라우트에 상관없이 모두에게 렌더링된다.
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/new" element={<New />} />
+      <Route path="/diary" element={<Diary />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+}
+
+export default App;
