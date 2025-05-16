53b73eca724e25963da436aba91def55bf9dc94c
sojeongw <sojeongw.official@gmail.com>
2025-05-16 12:51:30 +0900

    12.4. 페이지 이동하기

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -2,22 +2,35 @@ import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
 import NotFound from "./pages/NotFound";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
 
 // "/": 모든 일를 조회하는 home
 // "/new": 새로운 일기를 작성
 // "/diary": 일기를 상세히 조회
 
 function App() {
+  const nav = useNavigate();
+
+  const onClickButton = () => {
+    nav("/new");
+  };
   return (
     // routes 컴포넌트 안에는 route 컴포넌트만 들어갈 수 있다.
     // routes 밖에 있는 컴포넌트는 라우트에 상관없이 모두에게 렌더링된다.
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/new" element={<New />} />
-      <Route path="/diary" element={<Diary />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <>
+      <div>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/new"}>New</Link>
+        <Link to={"/diary"}>Diary</Link>
+      </div>
+      <button onClick={onClickButton}>New 페이지로 이동</button>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/diary" element={<Diary />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   );
 }
 
