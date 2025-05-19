90e502b8279e72c1f2cd7fb6b7de6835625f21e9
sojeongw <sojeongw.official@gmail.com>
2025-05-19 15:23:42 +0900

    12.7. 공통 컴포넌트 구현하기

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -2,36 +2,45 @@ import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
 import NotFound from "./pages/NotFound";
-import { Routes, Route, Link, useNavigate } from "react-router-dom";
-import { getEmotionImage } from "./util/get-emotion-image";
+import { Routes, Route } from "react-router-dom";
+import Button from "./components/Button";
+import Header from "./components/Header";
 
 // "/": 모든 일를 조회하는 home
 // "/new": 새로운 일기를 작성
 // "/diary": 일기를 상세히 조회
 
 function App() {
-  const nav = useNavigate();
-
-  const onClickButton = () => {
-    nav("/new");
-  };
   return (
     // routes 컴포넌트 안에는 route 컴포넌트만 들어갈 수 있다.
     // routes 밖에 있는 컴포넌트는 라우트에 상관없이 모두에게 렌더링된다.
     <>
-      <div>
-        <img src={getEmotionImage(1)} />
-        <img src={getEmotionImage(2)} />
-        <img src={getEmotionImage(3)} />
-        <img src={getEmotionImage(4)} />
-        <img src={getEmotionImage(5)} />
-      </div>
-      <div>
-        <Link to={"/"}>Home</Link>
-        <Link to={"/new"}>New</Link>
-        <Link to={"/diary"}>Diary</Link>
-      </div>
-      <button onClick={onClickButton}>New 페이지로 이동</button>
+      <Header
+        title={"Header"}
+        leftChild={<Button text={"Left"} />}
+        rightChild={<Button text={"Right"} />}
+      />
+      <Button
+        text={"123"}
+        type={"DEFAULT"}
+        onClick={() => {
+          console.log("버튼 클릭");
+        }}
+      />
+      <Button
+        text={"123"}
+        type={"POSITIVE"}
+        onClick={() => {
+          console.log("버튼 클릭");
+        }}
+      />
+      <Button
+        text={"123"}
+        type={"NEGATIVE"}
+        onClick={() => {
+          console.log("버튼 클릭");
+        }}
+      />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
