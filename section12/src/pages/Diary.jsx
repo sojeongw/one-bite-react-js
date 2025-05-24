e0eeabd3c2d5847118e57f79086a55903bf6a255
sojeongw <sojeongw.official@gmail.com>
2025-05-24 14:40:26 +0900

    12.16. Diary 페이지 구현하기

--- section12/src/pages/Diary.jsx
+++ section12/src/pages/Diary.jsx
@@ -1,9 +1,24 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import Viewer from "../components/Viewer";
 
 const Diary = () => {
   const params = useParams();
+  const nav = useNavigate();
 
-  return <div>{params.id}번 일기입니다.</div>;
+  return (
+    <div>
+      <Header
+        title={"yyyy-mm-dd 기록"}
+        leftChild={<Button onClick={() => nav(-1)} text={"< 뒤로 가기"} />}
+        rightChild={
+          <Button onClick={() => nav(`/edit/${params.id}`)} text={"수정하기"} />
+        }
+      />
+      <Viewer />
+    </div>
+  );
 };
 
 export default Diary;
